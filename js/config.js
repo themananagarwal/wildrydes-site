e4bf770adc4890b5ec22602ab2fa11e7b5fa7922
themananagarwal <mananmanoj.agarwal@utdallas.edu>
2025-04-27 17:59:12 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_H24nywlls', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '52dp71hjcha334qm8jmbdculev', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'us-east-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
